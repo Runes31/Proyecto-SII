a6fae113422a8b2e84b28d4f59b5c7c36c9d9892
JEfrain4 <47945869+JEfrain4@users.noreply.github.com>
2021-04-05 17:38:03 +0200

    Centro

---
+++ src/main/java/domain/Centro.java
@@ -0,0 +1,17 @@
+package domain;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Centro
+ *
+ */
+@Entity
+
+public class Centro {
+
+	public Centro() {
+		super();
+	}
+   
+}
