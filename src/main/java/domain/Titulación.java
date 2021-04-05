03d43089f7297e916702a986eec292b5a0490290
JEfrain4 <47945869+JEfrain4@users.noreply.github.com>
2021-04-05 17:32:57 +0200

    Añadir Entidad Titulación

---
+++ src/main/java/domain/Titulación.java
@@ -0,0 +1,84 @@
+package domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Titulación {
+   
+  @Id 
+  private String codigo;
+  @Column(nullable = false)
+  private String nombre;
+  @Column(nullable = false)
+  private int creditos;
+
+	public Titulación() {
+		super();
+	}
+
+  public String getCodigo() {
+    return codigo;
+  }
+
+  public void setCodigo(String codigo) {
+    this.codigo = codigo;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public int getCreditos() {
+    return creditos;
+  }
+
+  public void setCreditos(int creditos) {
+    this.creditos = creditos;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+    result = prime * result + creditos;
+    result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Titulación other = (Titulación) obj;
+    if (codigo == null) {
+      if (other.codigo != null)
+        return false;
+    } else if (!codigo.equals(other.codigo))
+      return false;
+    if (creditos != other.creditos)
+      return false;
+    if (nombre == null) {
+      if (other.nombre != null)
+        return false;
+    } else if (!nombre.equals(other.nombre))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "Titulación [codigo=" + codigo + ", nombre=" + nombre + ", creditos=" + creditos + "]";
+  }
+	
+  
+   
+}
