d0c277dc1457c9786dcb35d5818c5b8c6a1b09d0
JEfrain4 <47945869+JEfrain4@users.noreply.github.com>
2021-04-19 11:16:16 +0200

    Requisito 006

--- src/main/java/ejb/ImportarDatos.java
+++ src/main/java/ejb/ImportarDatos.java
@@ -1,19 +1,28 @@
 package ejb;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import javax.ejb.Local;
+
+import com.opencsv.exceptions.CsvException;
 
 @Local
 public interface ImportarDatos {
   /**
    * Requisito 006
    * Importa los datos de los alumnos desde un fichero Excel o CSV
+   * @param pathFichero
+   * @throws IOException 
+   * @throws FileNotFoundException 
+   * @throws CsvException 
    */
-  public void importarAlumnos();
+  public void importarAlumnosCSV(String pathFichero) throws FileNotFoundException, IOException, CsvException;
   
   /**
    * Requisito 012
    * Importar los datos recopilados de las preferencias de los alumnos en la elección de grupos
    */
-  public void importarPreferencias();
+  public void importarPreferenciasCSV();
 
 }
--- src/main/java/ejb/ImportarDatosEJB.java
+++ src/main/java/ejb/ImportarDatosEJB.java
@@ -1,8 +1,18 @@
 package ejb;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+import domain.Alumno;
 
 @Stateless
 public class ImportarDatosEJB implements ImportarDatos{
@@ -11,13 +21,20 @@ public class ImportarDatosEJB implements ImportarDatos{
   private EntityManager em;
   
   @Override
-  public void importarAlumnos() {
-    // TODO Auto-generated method stub
+  public void importarAlumnosCSV(String pathFichero) throws FileNotFoundException, IOException, CsvException {
+    try (CSVReader reader = new CSVReader(new FileReader("pathFichero"))) {
+      List<String[]> l = reader.readAll();
+      for(String[] str : l) {
+        Alumno al = new Alumno(str[0].toString(),str[1].toString(),str[2].toString(),str[3].toString(),str[4].toString(),str[5].toString());
+        em.persist(al);
+      }
+    }
+    
     
   }
 
   @Override
-  public void importarPreferencias() {
+  public void importarPreferenciasCSV() {
     // TODO Auto-generated method stub
     
   }
