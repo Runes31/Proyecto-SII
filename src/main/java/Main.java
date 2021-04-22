2368eb298835822b4d3bb885c3a2009e01114c86
JEfrain4 <47945869+JEfrain4@users.noreply.github.com>
2021-04-22 09:18:36 +0200

    Modificación

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 public class Main {
-  public static void main(String[] args){
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("Proyecto-SII");
-    emf.createEntityManager();
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
   }
+
 }
