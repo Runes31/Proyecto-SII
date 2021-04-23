23e4fda48bd5028f950d02eed28292fff97154ec
Alejandro Naranjo <naranjonalejandro@gmail.com>
2021-04-23 21:06:45 +0200

    added methods for testing purposes

--- src/main/java/ejb/GestionAlumno.java
+++ src/main/java/ejb/GestionAlumno.java
@@ -1,5 +1,6 @@
 package ejb;
 
+import java.util.List;
 import javax.ejb.Local;
 import domain.Alumno;
 import exceptions.AlumnoNoEncontradoException;
@@ -12,4 +13,18 @@ public interface GestionAlumno {
    * @throws AlumnoNoEncontradoException
    */
   public void actualizarAlumno(Alumno alumno) throws AlumnoNoEncontradoException;
+
+  /**
+   * Encuentra a un alumno por su pk
+   * @param id Id del alumno
+   * @return Objeto tipo Alumno
+   * @throws AlumnoNoEncontradoException
+   */
+  public Alumno findAlumno(int id) throws AlumnoNoEncontradoException;
+
+  /**
+   * Genera una lista con todos los alumnos
+   * @return List con los alumnos
+   */
+  public List<Alumno> getAllAlumnos();
 }
--- src/main/java/ejb/ModificarAlumnoEJB.java
+++ src/main/java/ejb/ModificarAlumnoEJB.java
@@ -1,5 +1,7 @@
 package ejb;
 
+import domain.Matricula;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.ejb.Stateless;
@@ -8,6 +10,10 @@ import javax.persistence.PersistenceContext;
 
 import domain.Alumno;
 import exceptions.AlumnoNoEncontradoException;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 @Stateless
 public class ModificarAlumnoEJB implements GestionAlumno {
@@ -19,9 +25,25 @@ public class ModificarAlumnoEJB implements GestionAlumno {
   
   @Override
   public void actualizarAlumno(Alumno alumno) throws AlumnoNoEncontradoException {
-    Alumno p=em.find(Alumno.class, alumno);
+    Alumno p=em.find(Alumno.class, alumno.getId());
     if(p==null) throw new AlumnoNoEncontradoException();
     em.merge(alumno);
   }
 
+  @Override
+  public Alumno findAlumno(int id) throws AlumnoNoEncontradoException {
+    Alumno a = em.find(Alumno.class, id);
+    if(a == null) throw new AlumnoNoEncontradoException();
+    return a;
+  }
+
+  @Override
+  public List<Alumno> getAllAlumnos(){
+    CriteriaBuilder cb = em.getCriteriaBuilder();
+    CriteriaQuery<Alumno> cq = cb.createQuery(Alumno.class);
+    Root<Alumno> rootEntry = cq.from(Alumno.class);
+    CriteriaQuery<Alumno> all = cq.select(rootEntry);
+    TypedQuery<Alumno> allQuery = em.createQuery(all);
+    return allQuery.getResultList();
+  }
 }
