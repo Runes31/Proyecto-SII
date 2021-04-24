dc686b443921353e3b7e96dd0649929cf127b0d9
Alejandro Naranjo <naranjonalejandro@gmail.com>
2021-04-24 22:43:45 +0200

    fixed importarDatos and added importarGrupos

--- src/main/java/ejb/ImportarDatos.java
+++ src/main/java/ejb/ImportarDatos.java
@@ -1,51 +1,42 @@
 package ejb;
 
+import com.opencsv.exceptions.CsvException;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
-
 import javax.ejb.Local;
-
-import com.opencsv.exceptions.CsvException;
 
 @Local
 public interface ImportarDatos {
   
   /**
-   * Importa los datos de los alumnos desde un fichero Excel o CSV
-   * Además importa las preferecnias de los alumnos en la elección de grupos
+   * Importa los datos de los alumnos desde un fichero Excel
    * @param fichero
-   * @throws IOException 
-   * @throws FileNotFoundException 
-   * @throws CsvException 
-   * @throws ParseException 
    */
-  public void importarAlumnosCSV(File fichero) throws IOException, CsvException, ParseException;
-  
-  public void importarAlumnosExcel(File fichero);
-  
-  /**
-   * Importa los datos de las titulaciones en CSV o Excel
-   * @param fichero
-   * @throws IOException 
-   * @throws CsvException 
-   * @throws FileNotFoundException 
-   */
-  public void importarTitulacionCSV(File fichero) throws IOException, CsvException;
-  
-  public void importarTitulacionExcel(File fichero);
-  
-  /**
-   * Importa las asignaturas ofertadas en CSV o Excel
-   * @param fichero
-   * @throws IOException 
-   * @throws CsvException 
-   * @throws FileNotFoundException 
-   */
-  
-  public void importarAsignaturasExcel(File fichero);
-  
-  public void importarEncuestaExcel(File fichero);
+  void importarAlumnosExcel(File fichero);
 
+  /**
+   * Importa los datos de las titulaciones en Excel
+   * @param fichero
+   */
+  void importarTitulacionExcel(File fichero);
+  
+  /**
+   * Importa las asignaturas ofertadas en Excel
+   * @param fichero
+   */
+  
+  void importarAsignaturasExcel(File fichero);
+
+  /**
+   * Importa las preferencias de los alumnos desde un excel
+   * @param fichero
+   */
+  void importarEncuestaExcel(File fichero);
+
+  /**
+   * Importa los grupos y las asignaturas que tienen
+   * @param fichero
+   */
+  void importarGruposExcel(File fichero);
 }
--- src/main/java/ejb/ImportarDatosEJB.java
+++ src/main/java/ejb/ImportarDatosEJB.java
@@ -1,38 +1,39 @@
 package ejb;
 
+import domain.Alumno;
+import domain.Asignatura;
+import domain.Asignatura.AsignaturaId;
+import domain.AsignaturasMatricula;
+import domain.Encuesta;
+import domain.Expediente;
+import domain.Grupo;
+import domain.GruposPorAsignatura;
+import domain.GruposPorAsignatura.GruposPorAsignaturaId;
+import domain.Matricula;
+import domain.Optativa;
+import domain.Titulacion;
+import exceptions.AsignaturaNoEncontradaException;
+import exceptions.ExpedienteNoEncontradoException;
+import exceptions.TitulacionNoEncontradaException;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.FileInputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
-import java.util.Arrays;
-import java.util.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;  
-import java.io.FileInputStream;  
-import java.util.Iterator; 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
-
-import domain.Alumno;
-import domain.Asignatura;
-import domain.Encuesta;
-import domain.Expediente;
-import domain.Matricula;
-import domain.Optativa;
-import domain.Titulacion;
-import exceptions.ExpedienteNoEncontradoException;
-import exceptions.TitulacionNoEncontradaException;
 
 @Stateless
 public class ImportarDatosEJB implements ImportarDatos{
@@ -43,67 +44,15 @@ public class ImportarDatosEJB implements ImportarDatos{
   private EntityManager em;
   
   @Override
-  public void importarAlumnosCSV(File fichero) throws FileNotFoundException, IOException, CsvException, ParseException {
-    String str;
-    String cursoAcademido = "", estadoMatricula = "";
-    try (CSVReader reader = new CSVReader(new FileReader(fichero))) {
-      List<String[]> l = reader.readAll();
-      for(int i = 0; i < l.size(); i++) {
-        if(i == 0) {
-          str = Arrays.toString(l.get(0));
-          String[] aux = str.split(";");
-          cursoAcademido = aux[1];
-        }
-        if(i == 2) {
-          str = Arrays.toString(l.get(1));
-          String[] aux = str.split(";");
-          estadoMatricula = aux[1];
-        }
-        if(i > 3) {
-          str = Arrays.toString(l.get(i));
-          String[] aux = str.split(";");
-          
-          Alumno al = new Alumno(aux[0].substring(1,aux[0].length()), aux[1] + " " + aux[2] + " " + aux[3], aux[6], aux[7], aux[8], aux[9]);
-          al.setDireccion(aux[10]);
-          al.setLocalidad(aux[11]);
-          al.setProvincia(aux[12]);
-          al.setCodigoPostal(aux[13]);
-          
-          Expediente exp = new Expediente((int) Double.parseDouble(aux[4]), true, Double.parseDouble(aux[17]));   
-          exp.setCreditosSuperados((int) Double.parseDouble(aux[18]));
-          exp.setCreditosFB((int) Double.parseDouble(aux[19]));
-          exp.setCreditosOB((int) Double.parseDouble(aux[20]));
-          exp.setCreditosOP((int) Double.parseDouble(aux[21]));
-          exp.setCreditosCF((int) Double.parseDouble(aux[22]));
-          exp.setCreditosPE((int) Double.parseDouble(aux[23]));
-          exp.setCreditosTF((int) Double.parseDouble(aux[24].substring(0,aux[24].length()-1)));
-          exp.setAlumno(al);          
-          
-          Matricula m = new Matricula();
-          m.setCursoAcademico(cursoAcademido);
-          m.setEstado(estadoMatricula);
-          m.setNumArchivo((int) Double.parseDouble(aux[5]));
-          m.setTurnoPreferente(aux[15]);
-          String fM = aux[14];
-          Date fechaMatricula = new SimpleDateFormat("dd/MM/yyyy").parse(fM);
-          m.setFechaMatricula(fechaMatricula);
-          m.setNuevoIngreso(false);
-          m.setListadoAsignaturas(aux[16]);
-          m.setExpediente(exp);
-          
-          em.merge(al);
-          em.merge(m);
-          em.merge(exp);
-        }
-        
-      }
-    }
-    
-  }
-  
-  @Override
   public void importarAlumnosExcel(File fichero) {
     try {
+      CriteriaBuilder cb = em.getCriteriaBuilder();
+      CriteriaQuery<Asignatura> cq = cb.createQuery(Asignatura.class);
+      Root<Asignatura> rootEntry = cq.from(Asignatura.class);
+      CriteriaQuery<Asignatura> all = cq.select(rootEntry);
+      TypedQuery<Asignatura> allQuery = em.createQuery(all);
+      List<Asignatura> asignaturas = allQuery.getResultList();
+
       FileInputStream file = new FileInputStream(fichero);
       XSSFWorkbook workbook = new XSSFWorkbook(file);
       XSSFSheet sheet = workbook.getSheetAt(0);
@@ -164,27 +113,25 @@ public class ImportarDatosEJB implements ImportarDatos{
         m.setExpediente(exp);
         em.persist(m);
 
+
+        for(String asignatura: m.getListadoAsignaturas().split(",")){
+          int dash = asignatura.lastIndexOf("-");
+          asignatura = asignatura.substring(0, (dash > 0) ? dash : asignatura.length());
+          int point = asignatura.lastIndexOf(".");
+          String c = asignatura.substring(0, (point > 0) ? point : asignatura.length())+".0";
+          Asignatura a = asignaturas.stream().filter(as -> as.getCodigo()
+              .equals(c)) .findFirst().orElse(null);
+          if(a == null) throw new AsignaturaNoEncontradaException("PK " + c + " y alumno " + al.getDni());
+          AsignaturasMatricula am = new AsignaturasMatricula();
+          am.setAsignatura(a);
+          am.setMatricula(m);
+          em.merge(am);
+        }
       }
       file.close();
     } catch (Exception e) { e.printStackTrace(); }    
   }
-  
-  public void importarTitulacionCSV(File fichero) throws IOException, CsvException {
-    try (CSVReader reader = new CSVReader(new FileReader(fichero))) {
-      List<String[]> l = reader.readAll();
-      for(int i = 1; i < l.size(); i++) {
-        Titulacion t = new Titulacion();
-        String str = Arrays.toString(l.get(i));
-        String[] aux = str.split(";");
-        t.setCodigo(aux[0]);
-        t.setNombre(aux[1]);
-        t.setCodigo(aux[2]);          
-        
-        em.merge(t);
-      }
-    }
-  }
-  
+
   public void importarTitulacionExcel(File fichero) {
     try {
       FileInputStream file = new FileInputStream(fichero);
@@ -223,11 +170,11 @@ public class ImportarDatosEJB implements ImportarDatos{
             Titulacion titulacion = new Titulacion();
             titulacion.setCodigo(cellIterator.next().toString());
             String codigo = titulacion.getCodigo();
-            if(codigo.equals("")) continue;
+            if(codigo.equals("")) break;
             titulacion = em.find(Titulacion.class, titulacion.getCodigo());
             if(titulacion == null) throw new TitulacionNoEncontradaException("No se ha encontrado la titulaciÃ³n con cÃ³digo " + codigo);
             asig.setTitulacion(titulacion);
-            asig.setOfertada(Boolean.parseBoolean(cellIterator.next().toString()));
+            asig.setOfertada(!cellIterator.next().toString().equalsIgnoreCase("no"));
             asig.setCodigo(cellIterator.next().toString());
             String referencia = cellIterator.next().toString();
             asig.setReferencia(referencia);
@@ -297,20 +244,76 @@ public class ImportarDatosEJB implements ImportarDatos{
 	      Iterator<Row> rowIterator = sheet.iterator();
 	      Row row = rowIterator.next();
 	      while (rowIterator.hasNext()) {
-	    	Encuesta e = new Encuesta();
+          Encuesta e = new Encuesta();
 	        row = rowIterator.next();
 	        Iterator<Cell> cellIterator = row.cellIterator();
-	        int numExpediente = Integer.valueOf(cellIterator.next().toString());
+          Date fechaMatricula = new SimpleDateFormat("dd-MM-yyyy").parse(cellIterator.next().toString());
+          e.setFechaEnvio(fechaMatricula);
+
+	        int numExpediente = (int) Double.parseDouble(cellIterator.next().toString());
 	        Expediente exp = em.find(Expediente.class, numExpediente);
 	        if(exp == null) throw new ExpedienteNoEncontradoException();
 	        e.setExpediente(exp);
-	        e.setGruposPorAsignaturas(null);
+	        String curso = cellIterator.next().toString();
+	        String asignaturas = cellIterator.next().toString();
+	        int grupo = (int) Double.parseDouble(cellIterator.next().toString());
+	        List<GruposPorAsignatura> gpa = new ArrayList<>();
+	        for(String asignatura: asignaturas.split(",")) {
+            GruposPorAsignaturaId pk = new GruposPorAsignaturaId(curso, new AsignaturaId(asignatura, exp.getTitulacion().getCodigo()), grupo);
+            gpa.add(em.find(GruposPorAsignatura.class, pk));
+          }
 
+	        e.setGruposPorAsignaturas(gpa);
 	        em.merge(e);
 	      }
 	      file.close();
 	    } catch (Exception e) { e.printStackTrace(); }    
 	  
   }
-  
+
+  @Override
+  public void importarGruposExcel(File fichero) {
+    try {
+      FileInputStream file = new FileInputStream(fichero);
+      XSSFWorkbook workbook = new XSSFWorkbook(file);
+      XSSFSheet sheet = workbook.getSheetAt(0);
+
+      Iterator<Row> rowIterator = sheet.iterator();
+      Row row = rowIterator.next();
+      List<GruposPorAsignatura> gpaList = new ArrayList<>();
+      while (rowIterator.hasNext()) {
+        Grupo g = new Grupo();
+        row = rowIterator.next();
+        Iterator<Cell> cellIterator = row.cellIterator();
+        g.setCurso(cellIterator.next().toString());
+        g.setLetra(cellIterator.next().toString());
+        g.setTurno(cellIterator.next().toString());
+        Titulacion t = em.find(Titulacion.class, cellIterator.next().toString());
+        if(t == null) throw new TitulacionNoEncontradaException();
+        g.setTitulacion(t);
+        String cursoAcademico = cellIterator.next().toString();
+        for(String asignatura: cellIterator.next().toString().split(",")) {
+          int point = asignatura.lastIndexOf(".");
+          asignatura = asignatura.substring(0, (point > 0) ? point : asignatura.length());
+          Asignatura a = em.find(Asignatura.class, new AsignaturaId(asignatura+ ".0", g.getTitulacion().getCodigo()));
+          if(a == null) throw new AsignaturaNoEncontradaException("Para la asignatura " + asignatura);
+          GruposPorAsignatura gpa = new GruposPorAsignatura();
+          gpa.setGrupo(g);
+          gpa.setAsignatura(a);
+          gpa.setCursoAcademico(cursoAcademico);
+          gpa.setOferta(true);
+          gpaList.add(gpa);
+        }
+        g.setIngles(cellIterator.next().toString().equals("Si"));
+        g.setVisible(cellIterator.next().toString().equals("Si"));
+        g.setAsignar(cellIterator.next().toString().equals("Si"));
+        g.setPlazas((int) Double.parseDouble(cellIterator.next().toString()));
+
+        em.persist(g);
+      }
+      gpaList.forEach(em::merge);
+      file.close();
+    } catch (Exception e) { e.printStackTrace(); }
+
+  }
 }
