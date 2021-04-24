fce11c7bd3b8cef302302ade81df5bbdd057bbe5
JEfrain4 <47945869+JEfrain4@users.noreply.github.com>
2021-04-24 12:24:00 +0200

    Update PruebaImportarDatos.java
    Prueba importar asignatura y alumno

--- src/test/java/ejb/PruebaImportarDatos.java
+++ src/test/java/ejb/PruebaImportarDatos.java
@@ -1,5 +1,8 @@
 package ejb;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
@@ -14,34 +17,68 @@ import org.junit.Test;
 
 import com.opencsv.exceptions.CsvException;
 
+import domain.Alumno;
 import domain.Asignatura;
+import exceptions.AlumnoNoEncontradoException;
 import exceptions.AsignaturaNoEncontradaException;
 
 public class PruebaImportarDatos {
+	
+	GestionAlumno ga;
+	GestionAsignatura gasi;
+	GestionExpediente ge;
+	GestionMatricula gestionMatricula;
   
   @Before
   public void setup() throws NamingException, FileNotFoundException, IOException, CsvException, ParseException  {
+	  ga = (GestionAlumno) SuiteTest.ctx.lookup("java:global/classes/ModificarAlumnoEJB");
+	  gasi = (GestionAsignatura) SuiteTest.ctx.lookup("java:global/classes/ModificarAsignaturaEJB");
+	  gestionMatricula = (GestionMatricula) SuiteTest.ctx.lookup("java:global/classes/GestionMatriculaEJB");
   }
   
-  @Test
+  /*@Test
+  @Requisitos({"006"})
   public void testImportarAlumnos() {
-    //Comprobar que las importaciones son correctas
-  }
+	  Alumno a = null;
+	  a = ga.getAllAlumnos().stream().findFirst().orElse(null);
+	  if(a == null) fail("No hay alumnos");
+	  String dni = "95115697E";
+	  a.setDni(dni);
+	  String nombre = "Carmelita Enriquez Navarro";
+	  a.setNombre(nombre);
+	  Alumno a2 = null;
+	  try {
+		a2 = ga.findAlumno(a.getId());
+	  } catch (AlumnoNoEncontradoException e) {
+	  		fail("Alumno no encontrado");
+	  }
+	  assertEquals(dni, a2.getDni());
+	  assertEquals(nombre, a2.getNombre());
+  }*/
   
   @Test
+  @Requisitos({"012"})
   public void testImportarTitulaciones() {
     //Comprobar que las importaciones son correctas    
   }
   
   @Test
+  @Requisitos({"015"})
   public void testImportarAsignaturas() throws AsignaturaNoEncontradaException, FileNotFoundException, IOException, CsvException, ParseException {
-//    EntityManagerFactory emf = Persistence.createEntityManagerFactory("Proyecto-SII");
-//    EntityManager em = emf.createEntityManager();
-//    BaseDatos.inicializaBaseDatos("Proyecto-SII");
-//    Asignatura asig = em.find(Asignatura.class, "50659");
-//    if(asig == null) throw new AsignaturaNoEncontradaException();
-//    else System.out.println(asig.toString());
-//    em.close();
-//    emf.close();
+	  Asignatura asig = null;
+	  asig = gasi.getAllAsignatura().stream().findFirst().orElse(null);
+	  if(asig == null) fail("No hay asignaturas");
+	  String codigo = "106";
+	  asig.setCodigo(codigo);
+	  String referencia = "50659";
+	  asig.setReferencia(referencia);
+	  Asignatura asig2 = null;
+	  try {
+		asig = gasi.findAsignatura(asig.getReferencia());
+	  } catch (AsignaturaNoEncontradaException e) {
+	  		fail("Asignatura no encontrado");
+	  }
+	  assertEquals(codigo, asig2.getCodigo());
+	  assertEquals(referencia, asig2.getReferencia());
   }
 }
