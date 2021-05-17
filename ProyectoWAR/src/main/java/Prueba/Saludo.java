fe01912eab2107c1c546dda9336120a17e6476ae
JEfrain4 <47945869+JEfrain4@users.noreply.github.com>
2021-05-17 17:12:10 +0200

    update

---
+++ ProyectoWAR/src/main/java/Prueba/Saludo.java
@@ -0,0 +1,14 @@
+package Prueba;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class Saludo {
+  public String getSaludo() {
+    return "Hola mundo";
+  }
+}
+
+}
