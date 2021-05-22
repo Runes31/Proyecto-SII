ccc9a3e6207d7b8c0ee78fcd131d558c4e065d88
Alejandro Naranjo <naranjonalejandro@gmail.com>
2021-05-22 20:16:54 +0200

    added base for frontend

---
+++ ProyectoWAR/src/main/java/Prueba/Importar.java
@@ -0,0 +1,51 @@
+package Prueba;
+
+import ejb.ImportarDatos;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Paths;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.Part;
+
+@ViewScoped
+@Named
+public class Importar {
+  private Part alumnos;
+  @Inject
+  ImportarDatos ejb;
+
+  public Part getAlumnos() {
+    return alumnos;
+  }
+
+  public void setAlumnos(Part alumnos) {
+    this.alumnos = alumnos;
+  }
+
+  public void process(){
+    if(alumnos != null) ejb.importarAlumnosExcel(getFile(alumnos));
+  }
+
+  private File getFile(Part part) {
+    String fileName = Paths.get(part.getSubmittedFileName()).getFileName().toString();
+    File f = null;
+    try(InputStream inputStream = part.getInputStream()) {
+      f = File.createTempFile("alumnos", "xlsx");
+      OutputStream outputStream = new FileOutputStream(f);
+      byte[] buffer = new byte[1024];
+      int length;
+      while ((length = inputStream.read(buffer)) > 0){
+        outputStream.write(buffer, 0, length);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return f;
+  }
+}
--- ProyectoWAR/src/main/java/Prueba/Saludo.java
+++ ProyectoWAR/src/main/java/Prueba/Saludo.java
@@ -1,12 +1,32 @@
 package Prueba;
 
-import javax.enterprise.context.RequestScoped;
+import java.io.Serializable;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+@ViewScoped
 @Named
-@RequestScoped
-public class Saludo {
-  public String getSaludo() {
-    return "Hola mundo";
+public class Saludo implements Serializable {
+  private String name;
+  private String clase;
+
+  public String getClase() {
+    return clase;
+  }
+
+  public void setClase(String clase) {
+    this.clase = clase;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String search(){
+    return name + " --- " + clase;
   }
 }
