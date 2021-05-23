49e00358f04fbf12beac01dcd77cc37a72c54b8f
JEfrain4 <47945869+JEfrain4@users.noreply.github.com>
2021-05-23 20:15:25 +0200

    Update Importar.java

--- ProyectoWAR/src/main/java/Prueba/Importar.java
+++ ProyectoWAR/src/main/java/Prueba/Importar.java
@@ -1,6 +1,9 @@
 package Prueba;
 
 import ejb.ImportarDatos;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -27,12 +30,42 @@ public class Importar {
     this.alumnos = alumnos;
   }
 
-  public void process(){
+  public void process() throws IOException{
     if(alumnos != null) ejb.importarAlumnosExcel(getFile(alumnos));
   }
 
-  private File getFile(Part part) {
-    String fileName = Paths.get(part.getSubmittedFileName()).getFileName().toString();
+  private File getFile(Part part) throws IOException {
+    
+    String filename = part.getSubmittedFileName();
+    String prefix = filename;
+    String suffix = "";
+    if (filename.contains("."))
+    {
+        prefix = filename.substring(0, filename.lastIndexOf('.'));
+        suffix = filename.substring(filename.lastIndexOf('.'));
+    }
+    File file = File.createTempFile("alumnos", "xlsx");
+    
+    InputStream input = null;
+    OutputStream output = null;
+    try {
+      input = new BufferedInputStream(part.getInputStream(), 2048);
+      output = new BufferedOutputStream(new FileOutputStream(file), 2048);
+      byte[] buffer = new byte[2048];
+      for (int length = 0; ((length = input.read(buffer)) > 0);){
+        output.write(buffer, 0, length);
+      }
+    } finally {
+      if (output != null)
+        try {output.close();} catch (IOException logOrIgnore) {}
+      if (input != null)
+        try {input.close();} catch (IOException logOrIgnore) {}
+    }
+    part.delete();
+    return file;
+    }
+   
+    /**String fileName = part.getSubmittedFileName();
     File f = null;
     try(InputStream inputStream = part.getInputStream()) {
       f = File.createTempFile("alumnos", "xlsx");
@@ -46,5 +79,5 @@ public class Importar {
       e.printStackTrace();
     }
     return f;
-  } 
-}
+    */
+  }
