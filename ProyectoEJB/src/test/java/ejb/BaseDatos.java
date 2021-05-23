e952459d3e57b9e9b9a52302f029a5e18b261da2
Alejandro Naranjo <naranjonalejandro@gmail.com>
2021-05-23 13:11:58 +0200

    fixed tests

--- ProyectoEJB/src/test/java/ejb/BaseDatos.java
+++ ProyectoEJB/src/test/java/ejb/BaseDatos.java
@@ -1,38 +1,24 @@
 package ejb;
 
+import com.opencsv.exceptions.CsvException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.text.ParseException;
-
 import javax.naming.NamingException;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-import com.opencsv.exceptions.CsvException;
-
-import ejb.ImportarDatosEJB;
 
 public class BaseDatos {
 
   public static void inicializaBaseDatos(String nombreUnidadPersistencia)
       throws FileNotFoundException, IOException, CsvException, ParseException, NamingException {
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory(nombreUnidadPersistencia);
-    EntityManager em = emf.createEntityManager();
-
     ImportarDatos importarDatos = (ImportarDatos) SuiteTest.ctx.lookup("java:global/classes/ImportarDatosEJB");
     
-    File t = new File("DATOS/Titulacion.xlsx");
+    File t = new File("../DATOS/Titulacion.xlsx");
     importarDatos.importarTitulacionExcel(t);
-    File file = new File("DATOS/Oferta_asignaturas.xlsx");
+    File file = new File("../DATOS/Oferta_asignaturas.xlsx");
     importarDatos.importarAsignaturasExcel(file);
-    importarDatos.importarAlumnosExcel(new File("DATOS/alumnos.xlsx"));
-    importarDatos.importarGruposExcel(new File("DATOS/grupos.xlsx"));
-    importarDatos.importarEncuestaExcel(new File("DATOS/Encuesta.xlsx"));
+    importarDatos.importarAlumnosExcel(new File("../DATOS/alumnos.xlsx"));
+    importarDatos.importarGruposExcel(new File("../DATOS/grupos.xlsx"));
+    importarDatos.importarEncuestaExcel(new File("../DATOS/Encuesta.xlsx"));
   }
 }
