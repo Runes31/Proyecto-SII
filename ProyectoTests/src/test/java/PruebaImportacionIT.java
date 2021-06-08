0b0aa54971c62d3682cf25d603baea3b44d47630
JEfrain4 <47945869+JEfrain4@users.noreply.github.com>
2021-06-08 20:23:14 +0200

    update importarIT

--- ProyectoTests/src/test/java/PruebaImportacionIT.java
+++ ProyectoTests/src/test/java/PruebaImportacionIT.java
@@ -35,13 +35,18 @@ public class PruebaImportacionIT {
 	File titulacion = new File("Titulacion.xlsx");
 	File alumnos = new File("alumnos.xlsx");
 	File asignaturas = new File("Oferta_asignaturas.xlsx");
+	File grupos = new File("grupos.xlsx");
+	File encuesta = new File("Encuesta.xlsx");
     driver.get("http://localhost:8080/ProyectoWAR/");
     driver.manage().window().setSize(new Dimension(1936, 1056));
     driver.findElement(By.linkText("Importar")).click();
     driver.findElement(By.id("Importaciones:Alumnos")).sendKeys(alumnos.getAbsolutePath());
     driver.findElement(By.id("Importaciones:Titulaciones")).sendKeys(titulacion.getAbsolutePath());
     driver.findElement(By.id("Importaciones:Asignaturas")).sendKeys(asignaturas.getAbsolutePath());
+    driver.findElement(By.id("Importaciones:Grupos")).sendKeys(grupos.getAbsolutePath());
+    driver.findElement(By.id("Importaciones:Encuestas")).sendKeys(encuesta.getAbsolutePath());
     driver.findElement(By.id("Importaciones:submit")).click();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    driver.findElement(By.linkText("Home")).click();
   }
 }
